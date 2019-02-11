6736985941736d2879480cdda70058fa74076818
Joshua Lee <configurellc@gmail.com>
2019-02-11 01:26:44 -0500

    This directory allows use to input your ES2015 code that is later compiled and traformed in the lib directory

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const hellWorld = () => 'hello';
+const heyWorld = () => 'hello';
+
+const templateLiteral = `${hellWorld()} I'm using template literals to make all this happen`;
+
+alert(templateLiteral);
